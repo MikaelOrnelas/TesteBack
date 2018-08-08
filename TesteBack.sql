15391b550bca45205be143e15f2660fc64e5d865
Mikael Ornelas <mikael.ornelas@hotmail.com>
2018-08-08 02:33:36 -0300

    Correcao codigo sql

--- TesteBack.sql
+++ TesteBack.sql
@@ -3,7 +3,7 @@ drop table tb_customer_account
 create table tb_customer_account (
 	id_customer int NOT NULL,
 	cpf_cnpj numeric,
-	nm_customer char(20),
+	nm_customer varchar(20),
 	is_active bit,
 	vl_total float
 	PRIMARY KEY(id_customer)
